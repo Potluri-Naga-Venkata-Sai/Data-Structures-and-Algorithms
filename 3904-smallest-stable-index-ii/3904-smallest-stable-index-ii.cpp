77bb1c1752302d62d2106bda786f3cf385867180
Naga-Venkata-Sai-Potluri <149510744+Potluri-Naga-Venkata-Sai@users.noreply.github.com>
2026-09-05 14:49:17 +0530

    Time: 4 ms (89.3%), Space: 202.6 MB (76.32%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -5,14 +5,12 @@ public:
 
         vector<int> suffixMin(n);
 
-        // suffixMin[i] = min(nums[i ... n-1])
         suffixMin[n - 1] = nums[n - 1];
 
         for (int i = n - 2; i >= 0; i--) {
             suffixMin[i] = min(nums[i], suffixMin[i + 1]);
         }
 
-        // prefixMax = max(nums[0 ... i])
         int prefixMax = INT_MIN;
 
         for (int i = 0; i < n; i++) {
